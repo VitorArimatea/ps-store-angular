df9fd11f244f1649da161fff7e42d2d820293c40
Vitor Arimatea <vitorodev@outlook.com>
2023-09-21 10:33:18 -0300

    new: Page Discover, Components navbar, game-card

---
+++ src/app/components/game-card/card-prices/card-prices.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-prices',
+  templateUrl: './card-prices.component.html',
+  styleUrls: ['./card-prices.component.css']
+})
+export class CardPricesComponent {
+
+}
