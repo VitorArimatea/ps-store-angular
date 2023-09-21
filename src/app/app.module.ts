b2da1e7d910644b2534ceee39551c5c2db273607
Vitor Arimatea <vitorodev@outlook.com>
2023-09-21 10:35:32 -0300

    update: Components import and adding them

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DiscoverComponent } from './pages/discover/discover.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { GameCardComponent } from './components/game-card/game-card.component';
+import { CardLabelComponent } from './components/game-card/card-label/card-label.component';
+import { CardPricesComponent } from './components/game-card/card-prices/card-prices.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DiscoverComponent,
+    NavbarComponent,
+    GameCardComponent,
+    CardLabelComponent,
+    CardPricesComponent
   ],
   imports: [
     BrowserModule,
